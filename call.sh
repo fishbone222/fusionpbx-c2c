81f80c063eab9720b434756f4884e631bef31506
fishbone222 <github@fischer-siegen.de>
2021-09-30 20:35:21 +0200

    first_test

--- call.sh
+++ call.sh
@@ -1,12 +1,12 @@
-server="fusionpbx.${hostname -d}
-src_cid_name=
-src_cid_number=
+server="fusionpbx.$(hostname -d)
+dest=$1
+key=$2
+src_cid_name="c2c"
+src_cid_number=$dest
 dest_cid_name=
 dest_cid_number=
-src=
-dest=
+src="150"
 auto_answer=false
 rec=false
 ringback=de-ring
-key={api-key}
-curl "https://$server/app/click_to_call/click_to_call.php?src_cid_name=$src_cid_name&src_cid_number=$src_cid_number&dest_cid_name=$dest_cid_name&dest_cid_number=$dest_cid_number&src=$src&dest=$dest&auto_answer=$auto_answer&rec=$rec&ringback=$ringback&key=$key"
+curl -G -v "https://$server/app/click_to_call/click_to_call.php" --data-urlencode "src_cid_name=$src_cid_name" --data-urlencode "src_cid_number=$src_cid_number" --data-urlencode "dest_cid_name=$dest_cid_name" --data-urlencode "dest_cid_number=$dest_cid_number" --data-urlencode "src=$src" --data-urlencode "dest=$dest" --data-urlencode "auto_answer=$auto_answer" --data-urlencode "rec=$rec" --data-urlencode "ringback=$ringback" --data-urlencode "key=$key"

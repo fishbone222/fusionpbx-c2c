c54af9c94864de682d78c894a1c00ab668a0f1c3
fishbone222 <github@fischer-siegen.de>
2021-10-04 13:13:35 +0200

    moved settings, added close

--- fusionpbx-c2c.sh
+++ fusionpbx-c2c.sh
@@ -1,12 +1,4 @@
-source ./fusionpbx-c2c.key
+source $(dirname "$0")/fusionpbx-c2c.settings
 server="fusionpbx.$(hostname -d)"
 dest=$1
-src_cid_name="c2c"
-src_cid_number=$dest
-dest_cid_name=
-dest_cid_number=
-src="150"
-auto_answer=false
-rec=false
-ringback=de-ring
-curl -G -v "https://$server/app/click_to_call/click_to_call.php" --data-urlencode "src_cid_name=$src_cid_name" --data-urlencode "src_cid_number=$src_cid_number" --data-urlencode "dest_cid_name=$dest_cid_name" --data-urlencode "dest_cid_number=$dest_cid_number" --data-urlencode "src=$src" --data-urlencode "dest=$dest" --data-urlencode "auto_answer=$auto_answer" --data-urlencode "rec=$rec" --data-urlencode "ringback=$ringback" --data-urlencode "key=$key"
+curl -G -v "https://$server/app/click_to_call/click_to_call.php" --data-urlencode "src_cid_name=$src_cid_name" --data-urlencode "src_cid_number=$src_cid_number" --data-urlencode "dest_cid_name=$dest_cid_name" --data-urlencode "dest_cid_number=$dest_cid_number" --data-urlencode "src=$src" --data-urlencode "dest=$dest" --data-urlencode "auto_answer=$auto_answer" --data-urlencode "rec=$rec" --data-urlencode "ringback=$ringback" --data-urlencode "key=$key" -H "Connection: close"
